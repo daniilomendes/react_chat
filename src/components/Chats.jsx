8478613358d71da3467fe39e8fcb9351d0c9ba6c
Danilo Mendes <danilo.mirandha@gmail.com>
2024-02-14 21:28:34 -0300

    feat: add front do Chats component

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const Chats = () => {
   return (
-    <div>Chats</div>
-  )
-}
+    <div className="chats">
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/19539378/pexels-photo-19539378/free-photo-of-bolsa-sacola-alforje-valise.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Danilo</span>
+          <p>hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/19539378/pexels-photo-19539378/free-photo-of-bolsa-sacola-alforje-valise.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Danilo</span>
+          <p>hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/19539378/pexels-photo-19539378/free-photo-of-bolsa-sacola-alforje-valise.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Danilo</span>
+          <p>hello</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Chats
+export default Chats;
