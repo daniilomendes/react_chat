3a8f4d1ea5778ffc2854ff32e6e26228d5ff247f
Danilo Mendes <danilo.mirandha@gmail.com>
2024-02-14 21:30:45 -0300

    setup: export do components

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import  Chat  from "./Chat";
+import  Chats  from "./Chats";
+import  Input  from "./Input";
+import  Message  from "./Message";
+import  Navbar  from "./Navbar";
+import  Search  from "./Search";
+import  Sidebar  from "./Sidebar";
+import  Messages  from "./Messages";
+
+export { Chat, Chats, Input, Message, Navbar, Search, Sidebar, Messages };
