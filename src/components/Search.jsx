a60e5cbe2d26b74d63b906c53b5d5d9e186e2793
Danilo Mendes <danilo.mirandha@gmail.com>
2024-02-14 21:29:32 -0300

    feat: add front do Search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Search = () => {
   return (
-    <div>Search</div>
-  )
-}
+    <div className="search">
+      <div className="searchForm">
+        <input type="text" placeholder="Buscar um usuário" />
+      </div>
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/19539378/pexels-photo-19539378/free-photo-of-bolsa-sacola-alforje-valise.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Danilo</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Search
+export default Search;
