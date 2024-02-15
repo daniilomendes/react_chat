0b37b2eae8c8e6d302e914ed80ada24c10fa72d8
Danilo Mendes <danilo.mirandha@gmail.com>
2024-02-14 21:29:22 -0300

    feat: add front do Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <span className="logo">Chat Online</span>
+      <div className="user">
+        <img src="https://images.pexels.com/photos/19539378/pexels-photo-19539378/free-photo-of-bolsa-sacola-alforje-valise.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+        <span>Danilo</span>
+        <button>Sair</button>
+      </div>
+    </div>
   )
 }
 
