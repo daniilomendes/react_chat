7fec455641db145d9e314ede32d71cb6cacd2eb2
Danilo Mendes <danilo.mirandha@gmail.com>
2024-02-14 21:28:26 -0300

    feat: add front do Chat component

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import Messages from "./Messages";
+import  Input  from "./Input";
+
 
 const Chat = () => {
   return (
-    <div>Chat</div>
-  )
-}
+    <div className="chat">
+      <div className="chatInfo">
+        <span>Danilo</span>
+        <div className="chatIcons">
+          <img src="/cam.png" alt="" />
+          <img src="/add.png" alt="" />
+          <img src="/more.png" alt="" />
+        </div>
+      </div>
+      <Messages />
+      <Input />
+    </div>
+  );
+};
 
-export default Chat
+export default Chat;
