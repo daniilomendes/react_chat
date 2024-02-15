1770fafb0bd52d67f890b1d75fe481efd1962f85
Danilo Mendes <danilo.mirandha@gmail.com>
2024-02-14 21:28:58 -0300

    feat: add front do Message component

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className='message owner'>
+      <div className="messageInfo">
+        <img src="https://images.pexels.com/photos/19539378/pexels-photo-19539378/free-photo-of-bolsa-sacola-alforje-valise.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+        <span>agora</span>
+      </div>
+      <div className="messageContent">
+        <p>Hello</p>
+        <img src="https://images.pexels.com/photos/19539378/pexels-photo-19539378/free-photo-of-bolsa-sacola-alforje-valise.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+      </div>
+    </div>
   )
 }
 
