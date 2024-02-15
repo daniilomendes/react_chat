f722fbe8b0401c7536da67c74b4756ae71094ae8
Danilo Mendes <danilo.mirandha@gmail.com>
2024-02-14 21:28:48 -0300

    feat: add front do Input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Input = () => {
   return (
-    <div>Input</div>
+    <div className='input'>
+      <input type="text" placeholder='Diga algo...' />
+      <div className="send">
+        <img src="/attach.png" alt="" />
+        <input type="file" style={{display: 'none'}} id='file' />
+        <label htmlFor="file">
+          <img src="img.png" alt="" />
+        </label>
+        <button>Enviar</button>
+      </div>
+    </div>
   )
 }
 
